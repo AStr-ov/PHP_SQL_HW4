adbfbb0d38881faa3f1b84824c8e268b07e141ee
Astr <boxreg2000@yandex.ru>
2022-07-01 17:16:02 +0300

    ДЗ №4 SQL

---
+++ HomeWork №4/sql4.sql
@@ -0,0 +1,15 @@
+CREATE TABLE STUDENTS (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  age INTEGER NOT NULL,
+  address TEXT NOT NULL
+);
+
+-- insert
+INSERT INTO STUDENTS VALUES (0001, 'Иван', '20','Тула');
+INSERT INTO STUDENTS VALUES (0002, 'Анна', '20', 'Москва');
+INSERT INTO STUDENTS VALUES (0003, 'Влад', '18', 'Псков');
+INSERT INTO STUDENTS VALUES (0004, 'Борис', '51', 'Санкт Петербург');
+INSERT INTO STUDENTS VALUES (0005, 'Ирина', '23','Москва');
+INSERT INTO STUDENTS VALUES (0006, 'Алексей', '37','Ростов-на-Дону');
+INSERT INTO STUDENTS VALUES (0007, 'Юлия', '28','Санкт Петербург');
